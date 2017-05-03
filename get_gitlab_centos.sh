231d647687c25a9fc55644a7c78de3df9c69eda2
Vlad Gorbunov <vadikgo@gmail.com>
2017-05-03 17:37:00 +0300

    disable gpg check for gitlab repo

--- get_gitlab_centos.sh
+++ get_gitlab_centos.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+set -e
 mkdir -p /rpm
+
 curl -s https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | bash
+sed -i 's/repo_gpgcheck=1/repo_gpgcheck=0/g' /etc/yum.repos.d/gitlab_gitlab-ce.repo
 yum install --downloadonly --downloaddir=/tmp gitlab-ce -y
 
 curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-ci-multi-runner/script.rpm.sh | bash
+sed -i 's/repo_gpgcheck=1/repo_gpgcheck=0/g' /etc/yum.repos.d/runner_gitlab-ci-multi-runner.repo
 yum install --downloadonly --downloaddir=/tmp gitlab-ci-multi-runner -y
 
 mv /tmp/gitlab* /rpm
